6dbe21a5606babc47a98bd1a594e83b1e05450d4
Fajar Faturrahman <fajarfaturrahman06@gmail.com>
2019-08-30 11:19:45 +0700

    Tambahan Desain pendaftaran data diri dan tipe

--- app/src/main/java/com/extra/cyclyx/ui/pendaftaran/PendaftaranDataDiri.kt
+++ app/src/main/java/com/extra/cyclyx/ui/pendaftaran/PendaftaranDataDiri.kt
@@ -17,6 +17,8 @@ class PendaftaranDataDiri : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pendaftaran_data_diri, container, false)
