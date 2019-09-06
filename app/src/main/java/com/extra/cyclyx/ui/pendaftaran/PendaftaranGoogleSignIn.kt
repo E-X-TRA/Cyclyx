914ab2374a697295e57011fbc9b24b7dcef3648b
Fajar Faturrahman <fajarfaturrahman06@gmail.com>
2019-09-06 14:10:57 +0700

    commit aja dulu

--- app/src/main/java/com/extra/cyclyx/ui/pendaftaran/PendaftaranGoogleSignIn.kt
+++ app/src/main/java/com/extra/cyclyx/ui/pendaftaran/PendaftaranGoogleSignIn.kt
@@ -1,18 +1,33 @@
 package com.extra.cyclyx.ui.pendaftaran
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 
 import com.extra.cyclyx.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.fragment_pendaftaran_google_sign_in.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class PendaftaranGoogleSignIn : Fragment() {
+
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    lateinit var gso: GoogleSignInOptions
+    val RC_SIGN_IN: Int = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,7 +35,52 @@ class PendaftaranGoogleSignIn : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pendaftaran_google_sign_in, container, false)
+
+
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val signIn = sign_in_button as SignInButton
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(activity?.applicationContext!!,gso)
+        signIn.setOnClickListener {
+            signInGoogle()
+
+        }
+
+    }
+    private fun signInGoogle(){
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN){
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleResult(task)
+        }
+    }
+
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>){
+        try {
+            val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
+            updateUI(account)
+        }catch (e: ApiException){
+            Toast.makeText(activity?.applicationContext, e.toString(),  Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount?) {
+        val dispTxt = disptxt as TextView
+
+        dispTxt.text = account?.displayName
+    }
+
 
 
 }
