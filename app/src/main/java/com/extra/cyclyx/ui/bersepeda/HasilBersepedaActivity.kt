bdebebbf895ed39777bc7bf4acb9f2ddff853ec0
Alvaro Dwi <varoalvaro493@gmail.com>
2019-10-06 14:14:23 +0700

    Test Commit dari branch

---
+++ app/src/main/java/com/extra/cyclyx/ui/bersepeda/HasilBersepedaActivity.kt
@@ -0,0 +1,12 @@
+package com.extra.cyclyx.ui.bersepeda
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HasilBersepedaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hasil_bersepeda)
+    }
+}
