33b9bdf1840471eb02d74bf77e298e2a2e97f2ed
Vita Siswanto <raihanputraramd@gmail.com>
2020-01-08 09:46:54 +0700

    add segala

--- app/src/main/java/com/extra/cyclyx/MainActivity.kt
+++ app/src/main/java/com/extra/cyclyx/MainActivity.kt
@@ -2,8 +2,6 @@ package com.extra.cyclyx
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +10,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_main)
 
-        navView.setupWithNavController(navController)
+        //ERROR LAYOUT
+//        val navController = findNavController(R.id.nav_host_main)
+
+//        navView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/extra/cyclyx/ui/profil/ProfilFragment.kt
+++ app/src/main/java/com/extra/cyclyx/ui/profil/ProfilFragment.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.extra.cyclyx.R
 
 class ProfilFragment : Fragment() {
