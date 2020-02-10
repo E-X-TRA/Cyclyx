69f90d4d9de9d0fd7bb3fad174418dd90b4f2769
Fajar Faturrahman <52985772+FajarFaturrahman@users.noreply.github.com>
2020-02-10 13:33:17 +0700

    10/02/2020- Update Pengenalan

--- app/src/main/java/com/extra/cyclyx/ui/pengenalan/PengenalanFragment.kt
+++ app/src/main/java/com/extra/cyclyx/ui/pengenalan/PengenalanFragment.kt
@@ -56,7 +56,7 @@ class PengenalanFragment : Fragment() {
         mList.add(
             IntroItem(
                 "",
-                "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book",
+                "Cyclyx membuat para pesepeda lebih tahu manfaat apa saja yang didapatkan ketike bersepeda.",
                 R.drawable.mountain_2
             )
         )
